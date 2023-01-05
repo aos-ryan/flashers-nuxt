58ff28a1fa5594ba159bb7f2c3d4e2c468c6ca23
ryan <ryan@arrayofstars.com>
2023-01-05 12:12:50 -0500

    add boulders page link styling

--- pages/boulders.vue
+++ pages/boulders.vue
@@ -1,31 +1,66 @@
 <script setup>
-const { data } = await useAsyncGql('allBoulders');
+const { data } = await useAsyncGql("allBoulders");
 </script>
 
 <template>
-<div class="sidebar">
-<ul>
-  <li v-for="boulder in data?.boulderCollection.items">
-    <NuxtLink :to="(/boulders/ + boulder.sys.id)">{{boulder.name}}</NuxtLink>
-  </li>
-</ul>
-</div>
-<div class="main-content">
-  <NuxtPage />
-</div>
+  <div class="sidebar">
+    <ul>
+      <li v-for="boulder in data?.boulderCollection.items">
+        <NuxtLink
+          class="boulder_link"
+          :to="/boulders/ + boulder.sys.id"
+          >{{ boulder.name }}</NuxtLink
+        >
+      </li>
+    </ul>
+  </div>
+  <div class="main-content">
+    <NuxtPage />
+  </div>
 </template>
 
 <style scoped>
 .sidebar {
-  background-color:chartreuse;
+  background-color: chartreuse;
   width: 20%;
   height: 100vh;
   float: left;
+}
+li {
+  padding: 1rem 1rem;
 }
 .main-content {
   width: auto;
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.boulder_link {
+  text-decoration: none;
+  display: block;
+  font-size: 22px;
+  z-index: 1;
+  background-clip: text;
+  color: transparent;
+  background-image: linear-gradient(to right, #131313, #131313 50%, #fff 50%);
+  background-size: 200% 100%;
+  background-position: 100%;
+  transition: all 0.3s ease-in-out;
+}
+.boulder_link:before {
+  display: block;
+  content: "";
+  width: 0;
+  height: 3px;
+  bottom: 5px;
+  left: 0;
+  bottom: -3px;
+  z-index: 0;
+  position: absolute;
+  background: #23abd4;
+  transition: all 0.3s ease-in-out;
+}
+.boulder_link:hover {
+  background-position: 0%;
 }
 </style>
