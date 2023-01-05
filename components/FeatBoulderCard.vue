cc7aa5fbed12d2f75e6032932a0b703312153b37
ryan <ryan@arrayofstars.com>
2023-01-05 16:50:04 -0500

    add initial styling for featured boulder page and refactor into component

---
+++ components/FeatBoulderCard.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps({
+  name: String,
+  id: String,
+  grade: String,
+  url: String
+});
+</script>
+<template>
+  <article
+    class="boulder-card"
+    :key="id"
+  >
+    <div class="boulder-card-image">
+      <img :src="`${url}`" />
+    </div>
+    <div class="boulder-card-details">
+      <p>{{ name }}</p>
+      <p>{{ grade }}</p>
+    </div>
+  </article>
+</template>
+
+<style scoped>
+article {
+  height: 500px;
+  width: 100%;
+}
+img {
+  width: 350px;
+  height: 250px;
+}
+.boulder-card {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.boulder-card-details {
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,20 +5,16 @@ const { data } = await useAsyncGql("featuredBoulders");
 <template>
   <main>
     <h1>Featured Boulders</h1>
-    <div class="index-container">
-      <article
-        class="boulder-card"
-        v-for="boulder in data?.boulderCollection.items"
-        :key="boulder.sys.id"
-      >
-        <div class="boulder-card-image">
-          <img :src="`${boulder.image?.url}`" />
-        </div>
-        <div class="boulder-card-details">
-          <p>{{ boulder.name }}</p>
-          <p>{{ boulder.grade }}</p>
-        </div>
-      </article>
+    <div
+      class="index-container"
+      v-for="boulder in data?.boulderCollection.items"
+    >
+      <FeatBoulderCard
+        :name="boulder.name"
+        :id="boulder.sys.id"
+        :grade="boulder.grade"
+        :url="boulder.image?.url"
+      />
     </div>
   </main>
 </template>
@@ -30,16 +26,5 @@ const { data } = await useAsyncGql("featuredBoulders");
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
-}
-img {
-  width: 350px;
-  height: 250px;
-}
-.boulder-card {
-  display: flex;
-}
-.boulder-card-details {
-  position: absolute;
-  /* display: none; */
 }
 </style>
